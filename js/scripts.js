f9e050b1b0f2531bfcbfd2f7a3ca2b326cc73052
Tobiasz12 <tobiasz63@op.pl>
2017-03-27 22:28:34 +0200

    zadanie5

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+ getTriangleArea = function getTriangleArea(a, h) {
+
+	if ( (a <= 0) && (h <= 0) ) {
+	console.log('Nieprawidłowe dane')
+}
+	return a*h/2 ;
+}
+
+// zmienne pola trójkąta
+
+console.log( getTriangleArea(10,6) )
+console.log( getTriangleArea(5,6) )
+console.log( getTriangleArea(2,6) )
+
