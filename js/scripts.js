525132ae025b297fa58faf5568f5dffdf1ff9e77
Tobiasz12 <tobiasz63@op.pl>
2017-03-27 18:40:33 +0200

    zadanie4

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+
+ var a = 4;
+ var b = 4;
+ value = (a * a) + (2 * a * b) - (b * b)
+
+if ( value > 0 ) {
+	 console.log('Wynik jest dodatni');
+
+} else  {
+	 console.log('Wynik jest ujemny');
+
+}
+
+//did value is equal zero
+
+if ( value = 0 ) {
+	console.log('wynik jest równyt zero');
+
+} else {
+	console.log('wynik nie jest równay zero');
+}
+
