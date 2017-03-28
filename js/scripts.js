641ee32f02658fc21013f9aa0a25dd12761179d2
Tobiasz12 <tobiasz63@op.pl>
2017-03-28 18:18:02 +0200

    zadanie3

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 //triangleArea exmaple
 
-var triangleArea = prompt ('Pole trójkąta')
+
 var a = 8;
 var h = 4;
-triangleArea = a * h / 2;	
+var triangleArea = a * h / 2;	
 console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
