509423afe0f68fef0473df99976537e4fe5b602a
Tobiasz12 <tobiasz63@op.pl>
2017-03-29 22:43:15 +0200

    zad7

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+var text ='Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.';
+var animal = 'Zielone słonie';
+var animalUpperCased = animal.toUpperCase();
+var textCharsAfter = text.replace('Papugi', 'Zielone słonie');
+var partOfText = text.slice(0,40);
+
+console.log(partOfText);
+console.log(textCharsAfter);
+console.log(animalUpperCased);
+console.log(text.length/2)
+
+
+
