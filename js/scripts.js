d4b31c273b8571e60bf6f15eb4c1c19838cdf39e
Tobiasz12 <tobiasz63@op.pl>
2017-03-30 21:19:44 +0200

    zadanie8

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+var n = 1;
+function rysujChoinke(n) {
+	for ( var k = 0 ; k < n ; k++) {	
+  		var star = '';
+  		
+  		for (var J = k ; j < (n*2-1) ; J++){
+  			star += '*';
+  		}
+  	}		
+	
+}
+console.log(star)
