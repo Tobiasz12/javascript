8a55f1e232343fa4b1795a0923005e25296434d2
Tobiasz12 <tobiasz63@op.pl>
2017-04-02 19:04:11 +0200

    zadanie7

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,3 @@
-var text ='Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.';
-var animal = 'Zielone słonie';
-var animalUpperCased = animal.toUpperCase();
-var textCharsAfter = text.replace(theString, animalUpperCased);
-theString = text.split(" ")[1];	
-var partOfText = textCharsAfter.slice(0,text.length/2);
-console.log(partOfText);
-
-
-
-
-// kurde no nie wiem jak to zrobić uzywając jedynie String.prototype.split() w sensie wiem ale nie wiem jak zamienić 
-//pierwszy wyraz bez pomocy replace
 
 var text ='Papugi kosza trawe, unoszac sie nad ziemia 15 centymetrów, na czerwonych dywanach.'
 var animal = 'Zielone slonie';
@@ -20,3 +7,12 @@ var partOfText = text.slice(0,text.length/2);
 console.log(partOfText);
 
 
+// tutaj raz działa i raz nie działa raczej jest to zle. Ogólne to problem polega na zamianie pierwszego słowa bez podania go w split, bo tak to działa
+
+var text ='Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.';
+var animal = 'Zielone słonie';
+var animalUpperCased = animal.toUpperCase();
+var textCharsAfter = text.replace(theString, animalUpperCased);
+theString = text.split(" ");	
+var partOfText = textCharsAfter.slice(0,text.length/2);
+console.log(partOfText);
