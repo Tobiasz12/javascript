d22f2017c82e1f116714048048e264f6b0e910b7
Tobiasz12 <tobiasz63@op.pl>
2017-04-02 18:49:53 +0200

    zadanie9

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+
+var withButtonClass = document.getElementsByClassName("button");
+	for (var i = 0; i < withButtonClass.length; i++) {
+		function button(alert) {
+ 	   withButtonClass.innerText;
+	}	
+
+	alert(withButtonClass.innerText)
+	}
+
+console.log(withButtonClass);
+
+
