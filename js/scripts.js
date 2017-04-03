35655f1c554c4de1c09780d8df62a6323c520cd6
Tobiasz12 <tobiasz63@op.pl>
2017-04-03 19:24:17 +0200

    zadanie77

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,9 @@
 
-var text ='Papugi kosza trawe, unoszac sie nad ziemia 15 centymetrów, na czerwonych dywanach.'
+var text ='Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.';
+var fristWord = text.split(' ',1);
 var animal = 'Zielone slonie';
 var animalUpperCased = animal.toUpperCase();
-text = text.split('Papugi').join(animalUpperCased);
-var partOfText = text.slice(0,text.length/2);
-console.log(partOfText);
-
-
-// tutaj raz działa i raz nie działa raczej jest to zle. Ogólne to problem polega na zamianie pierwszego słowa bez podania go w split, bo tak to działa
-
-var text ='Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.';
-var animal = 'Zielone słonie';
-var animalUpperCased = animal.toUpperCase();
-var textCharsAfter = text.replace(theString, animalUpperCased);
-theString = text.split(" ");	
+var textCharsAfter = text.replace(fristWord, animalUpperCased);
 var partOfText = textCharsAfter.slice(0,text.length/2);
+
 console.log(partOfText);
