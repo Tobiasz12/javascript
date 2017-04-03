25212e9b93afb208572cfaa0bb65aac9fc9871f9
Tobiasz12 <tobiasz63@op.pl>
2017-04-04 00:27:22 +0200

    zadanie10

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+var list = document.getElementById("list");
+var add = document.getElementById('addElem')
+
+add.addEventListener('click', function() {
+	list.innerHTML += '<li>item</li>'
+
+document.getElementsByTagName("li").length;
+  
+});
+
+
