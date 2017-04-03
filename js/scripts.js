104c589adf0484fbab3557aa5af9274acf59492e
Tobiasz12 <tobiasz63@op.pl>
2017-04-03 16:35:48 +0200

    zad9

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,9 @@
 
 var withButtonClass = document.getElementsByClassName("button");
 	for (var i = 0; i < withButtonClass.length; i++) {
-		function button(alert) {
- 	   withButtonClass.innerText;
-	}	
-
-	alert(withButtonClass.innerText)
-	}
-
+}
+alert(withButtonClass)
+}
 console.log(withButtonClass);
 
 
