ea7c68a18bd187d5ca7d39d70961722f35cc6d2f
Tobiasz12 <tobiasz63@op.pl>
2017-04-06 21:32:20 +0200

    zadanie10

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,14 @@
-var i = 1;
 var list = document.getElementById("list");
-var add = document.getElementById('addElem');
+var add = document.getElementById('addElem')
+
 add.addEventListener('click', function() {
-  var itemsByTagName = document.getElementsByTagName("li");
-  list.innerHTML += '<li>item ' ++ i '</li>'
+	 var itemsByTagName = document.getElementsByTagName("li");
+	list.innerHTML += '<li> item 'itemsByTagName.length'</li>'
+
+  
 });
 
-
+console.log(list)
 
 
 
