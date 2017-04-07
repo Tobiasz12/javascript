71eb71ef24ca3bff5ec738b982561856eaf6084d
Tobiasz12 <tobiasz63@op.pl>
2017-04-07 15:58:54 +0200

    zad10

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,16 @@ var list = document.getElementById("list");
 var add = document.getElementById('addElem')
 
 add.addEventListener('click', function() {
-	 var itemsByTagName = document.getElementsByTagName("li");
-	list.innerHTML += '<li> item 'itemsByTagName.length'</li>'
+	 var itemsByTagName = document.getElementsByTagName("li").length -4;
+	list.innerHTML += '<li>item '+ itemsByTagName + '</li>'  
 
   
 });
 
 console.log(list)
+
+
+
 
 
 
